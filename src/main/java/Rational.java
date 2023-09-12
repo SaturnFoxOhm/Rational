6606953b58ab9d8408cfb0c3291ec0ab20d4d39d
Supakitt Surojanakul <81941466+SaturnFoxOhm@users.noreply.github.com>
2023-09-12 16:37:42 +0700

    Add files via upload

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -10,10 +10,13 @@ class Rational {
 
     Rational() {
         // to be completed
+        super();
     }
 
     Rational(long numerator, long denominator) throws Illegal { 
         // to be completed
+        this.numerator = numerator;
+        this.denominator = denominator;
     } 
 
     // find the reduce form 
@@ -46,6 +49,9 @@ class Rational {
      */
     public void subtract(Rational x) {
         // to be completed
+        numerator = (numerator * x.denominator) - (x.numerator * denominator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -54,6 +60,9 @@ class Rational {
      */
     public void multiply(Rational x) { 
         // to be completed
+        numerator = (numerator * x.numerator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -62,6 +71,9 @@ class Rational {
      */
     public void divide(Rational x) {
         // to be completed
+        numerator = (numerator * x.denominator);
+        denominator = (denominator * x.numerator);
+        simplestForm();
     }
 
     /***
@@ -71,7 +83,13 @@ class Rational {
      */
     public boolean equals(Object x) {
         // to be completed
-        return true; // TODO: This needs to be modified.
+        // TODO: This needs to be modified.
+        simplestForm();
+        ((Rational) x).simplestForm();
+        if(numerator == ((Rational) x).numerator && denominator == ((Rational) x).denominator){
+            return true;
+        }
+        return false;
     }
 
     /***
@@ -82,7 +100,26 @@ class Rational {
      */
     public long compareTo(Object x) {
         // to be completed
-        return -1; // TODO: this needs to be modified.
+        // TODO: this needs to be modified.
+        simplestForm();
+        ((Rational) x).simplestForm();
+        if(denominator > ((Rational) x).denominator){
+            return -1;
+        }
+        else if(denominator < ((Rational) x).denominator){
+            return 1;
+        }
+        else{
+            if(numerator < ((Rational) x).numerator){
+                return -1;
+            }
+            else if(numerator > ((Rational) x).numerator){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
     }
 
     /***
@@ -91,7 +128,8 @@ class Rational {
      */
     public String toString() { 
         // to be completed
-        return ""; // TODO: This needs to be modified.
+        simplestForm();
+        return numerator+"/"+denominator; // TODO: This needs to be modified.
     }
 
     public static void main(String[] args) {
